26bdd7e9924367960daf1f9f42785b810a00be85
u-kai <76635578+u-kai@users.noreply.github.com>
2023-08-10 20:15:56 +0900

    add fail test

---
+++ tests/text_box_test.py
@@ -0,0 +1,18 @@
+import unittest
+from src.components import TextBox,Text
+
+
+class TestTextBox(unittest.TestCase):
+    def test_複数のTextを保持可能(self):
+        box = TextBox()
+        box.add(Text("Hello World"))
+        box.add(Text("Good Bye"))
+
+        self.assertEquals(box.texts[0].str(),"Hello World")
+        self.assertEquals(box.texts[1].str(),"Good Bye")
+
+        
+
+    #def test_fontの種類を選択可能(self):
+    #    sut = TextBox()
+    #   s
