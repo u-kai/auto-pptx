0f3a228a9c7ed82e5132da4d3ee52be886009381
u-kai <76635578+u-kai@users.noreply.github.com>
2023-08-12 09:26:48 +0900

    clear main.py
    Successfully add multiple slide and pptx convertor

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ def main():
 
     filename = "hello.pptx"
     p = PPTX(filename)
-    slide = Slide()
-
+    title_slide = Slide.title_slide()
     box = TextBox()
     text = Text("Hello World")
     text.to_bold()
     text.change_size(28)
     box.add(text)
-
     text = Text("Good Bye")
     box.add(text)
+    title_slide.add_textbox(StartPoint(50, 50), Size(30, 30), box)
 
-    slide.add_textbox(StartPoint(50, 50), Size(30, 30), box)
+    title_and_content_slide = Slide.title_and_content()
     text = Text("Rust Good Language")
     text.to_bold()
     text.change_size(28)
@@ -27,9 +26,10 @@ def main():
     list.add_child_to(0, Text("C++ Good Language"))
     list.top(0).child(0).add_child(Text("C Good Language"))
 
-    slide.add_list_text(StartPoint(150, 150), Size(100, 500), list)
+    title_and_content_slide.add_list_text(StartPoint(150, 150), Size(100, 500), list)
 
-    p.add_slide(slide)
+    p.add_slide(title_slide)
+    p.add_slide(title_and_content_slide)
     p.save()
 
 
--- src/pptx.py
+++ src/pptx.py
@@ -4,21 +4,23 @@ from pptx import Presentation
 
 from src.slide import Slide
 from src.convertors.slide import SlideConvertor
+from src.convertors.presentation import PresentationConvertor
 
 
 class PPTX:
     def __init__(self, filename: str):
-        self.presentation = Presentation()
+        self.convertor = PresentationConvertor(Presentation())
         self.filename = filename
         self.page = 0
 
     def save(self):
-        self.presentation.save(self.filename)
+        self.convertor.save(self.filename)
 
     def add_slide(self, slide: Slide):
-        self.presentation.slides.add_slide(self.presentation.slide_layouts[0])
-        convertor = SlideConvertor(self.presentation.slides[self.page])
-        convertor.convert(slide)
+        self.convertor.add_slide(slide)
+        # self.presentation.slides.add_slide(self.presentation.slide_layouts[0])
+        # convertor = SlideConvertor(self.presentation.slides[self.page])
+        # convertor.convert(slide)
         self.page += 1
         return
 
