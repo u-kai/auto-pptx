f5eaaf6c68978f14b51d7ee42c8e5568f141aec0
u-kai <76635578+u-kai@users.noreply.github.com>
2023-08-15 15:40:01 +0900

    add examples

--- examples/main.py
+++ examples/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.pptx import PPTX
 from src.slide import Slide, StartPoint, Size
 from src.components import TextBox, Text, ListText
